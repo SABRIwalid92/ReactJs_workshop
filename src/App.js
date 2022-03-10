be21022361d7ad99a52e39264d2a380ba2039206
carch921 <walid.sabri@esprit.tn>
2022-03-10 01:36:33 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Container, Navbar, Nav, Form, Row , Col} from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="#home">Vote Now!</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">About Us</Nav.Link>
+            <Nav.Link href="#features">Events</Nav.Link>
+            <Nav.Link href="#pricing" style={{color:'orange'}}><big>Contribute</big></Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Form.Select aria-label="Default select example">
+  <option>Nominees for tonight are</option>
+  <option value="1">Abu One</option>
+  <option value="2">Miss Two</option>
+  <option value="3">$4D Three</option>
+</Form.Select>
+      <Container>
+        <Row>
+          <Col>
+        <Form.Floating className="mb-3">
+          <Form.Control
+            id="floatingInputCustom"
+            type="email"
+            placeholder="name@example.com"
+          />
+          <label htmlFor="floatingInputCustom">Email address</label>
+        </Form.Floating>
+        </Col>
+        <Col>
+        <Form.Floating>
+          <Form.Control
+            id="floatingPasswordCustom"
+            type="password"
+            placeholder="Password"
+          />
+          <label htmlFor="floatingPasswordCustom">Password</label>
+        </Form.Floating>
+        <Form.Floating>
+          <Form.Control
+            id="floatingPasswordCustom"
+            type="password"
+            placeholder="F***Password"
+          />
+          <label htmlFor="floatingPasswordCustom"><small><b>fake</b></small>Password</label>
+        </Form.Floating>
+        </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
